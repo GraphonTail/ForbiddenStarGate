0cdebeb016be952cade3f06c7266801d5dd3765b
GraphonTail <polevik.andrew.vsd@gmail.com>
2024-04-18 01:23:46 +0300

    GAS
    GAS завантажений та налаштований, с реалізацією ривка з покращенням здоров'я

---
+++ Source/FSG/FSGAttributeSet.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "AbilitySystemComponent.h"
+#include "FSGAttributeSet.generated.h"
+
+// Uses macros from AttributeSet.h
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+	GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
+
+
+UCLASS()
+class FSG_API UFSGAttributeSet : public UAttributeSet
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(BlueprintReadOnly, Category = "Attributes")
+	FGameplayAttributeData Health;
+	ATTRIBUTE_ACCESSORS(UFSGAttributeSet, Health);
+
+	UPROPERTY(BlueprintReadOnly, Category = "Attributes")
+	FGameplayAttributeData MaxHealth;
+	ATTRIBUTE_ACCESSORS(UFSGAttributeSet, MaxHealth);
+};
---
+++ Source/FSG/FSGGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FSGGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AFSGGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AFSGGameMode();
+};
+
+
+
